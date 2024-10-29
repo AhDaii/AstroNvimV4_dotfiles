f6c47bdc0c05d58e689b4f072b7617159cd33772
AhDaii <1171469399@qq.com>
2024-10-29 21:38:04 +0800

    update

--- lua/community.lua
+++ lua/community.lua
@@ -14,10 +14,9 @@ return {
   {
     "echasnovski/mini.indentscope",
     opts = {
-        symbol = "╎",
-        options = {try_as_border = false, indent_at_cursor = true}
+      symbol = "╎",
+      options = { try_as_border = false, indent_at_cursor = true }
     }
-  }
+  },
+
 }
-
-
